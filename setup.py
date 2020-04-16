d8a4c6bf05238c177c3b98ce1c7b9926a514a948
Ken Kundert <ken@theKunderts.net>
2020-04-16 14:52:01 -0700

    Normalize code.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open("README.rst", encoding="utf-8") as file:
+    readme = file.read()
+
 setup(
     name='totz',
     version='1.2.1',
     author='Ken Kundert',
     author_email='totz@nurdletech.com',
     description='Convert timezone of a time.',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
     license='GPLv3+',
     scripts=['totz'],
     install_requires=[
