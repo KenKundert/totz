ed18f6d2f866993c4cceeab0e05c5a1576390512
Ken Kundert <ken@theKunderts.net>
2021-08-17 13:59:03 -0700

    enable zip_safe in installer

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ with open("README.rst", encoding="utf-8") as file:
     readme = file.read()
 
 setup(
-    name='totz',
-    version='1.3.2',
-    author='Ken Kundert',
-    author_email='totz@nurdletech.com',
-    description='Convert timezone of a time.',
-    long_description=readme,
-    long_description_content_type='text/x-rst',
-    license='GPLv3+',
-    scripts=['totz'],
-    install_requires=[
+    name = 'totz',
+    version = '1.3.2',
+    author = 'Ken Kundert',
+    author_email = 'totz@nurdletech.com',
+    description = 'Convert timezone of a time.',
+    long_description = readme,
+    long_description_content_type = 'text/x-rst',
+    license = 'GPLv3+',
+    scripts = ['totz'],
+    install_requires = [
         'docopt',
         'inform>=1.4',
     ],
-    python_requires='>=3.6',
+    python_requires = '>=3.6',
+    zip_safe = True,
 )
